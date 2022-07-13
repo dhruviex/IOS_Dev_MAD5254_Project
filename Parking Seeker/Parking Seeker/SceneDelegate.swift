42b1b798b097b2b3d7a1c05e43188880cbfbe38a
parthvekariya98 <parthvekariya260898@gmail.com>
2022-07-13 03:19:44 -0400

    - navigation root view controller setup

--- Parking Seeker/Parking Seeker/SceneDelegate.swift
+++ Parking Seeker/Parking Seeker/SceneDelegate.swift
@@ -13,10 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        //Set root view controller for programmatic navigation
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let rootVC = storyboard.instantiateViewController(identifier: "WelcomeViewController") as? WelcomeViewController
+        let rootNC = UINavigationController(rootViewController: rootVC!)
+        self.window?.rootViewController = rootNC
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
