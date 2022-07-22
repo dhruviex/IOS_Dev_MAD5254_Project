3b004e8228fc83931131992620011e4f2b900e03
deekshith024 <deekshithreddy0001@gmail.com>
2022-07-22 15:18:18 -0400

    otp_viewcontroller

---
+++ Parking Seeker/Parking Seeker/OtpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OtpViewController.swift
+//  Parking Seeker
+//
+//  Created by Karthik Reddy Rondla on 2022-07-22.
+//
+
+import UIKit
+
+class OtpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
