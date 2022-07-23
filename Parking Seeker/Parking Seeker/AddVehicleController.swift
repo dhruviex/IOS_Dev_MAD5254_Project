073e5276d412f0e907564fd82a972e555db50881
parthvekariya98 <parthvekariya260898@gmail.com>
2022-07-22 20:19:46 -0400

    - ui design for add vehicle screen

---
+++ Parking Seeker/Parking Seeker/AddVehicleController.swift
@@ -0,0 +1,23 @@
+//
+//  AddVehicleController.swift
+//  Parking Seeker
+//
+//  Created by parth on 2022-07-22.
+//
+
+import UIKit
+
+class AddVehicleController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func backBtnTapped(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+
+}
