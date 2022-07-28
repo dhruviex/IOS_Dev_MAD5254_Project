fcd2d51cb18c4ebe24eb5aada88dfbbce9d1a3a6
parthvekariya98 <parthvekariya260898@gmail.com>
2022-07-28 18:12:27 -0400

    - ui design for add vehicle screen and manage selection dropdown for vehicle type

--- Parking Seeker/Parking Seeker/AddVehicleController.swift
+++ Parking Seeker/Parking Seeker/AddVehicleController.swift
@@ -6,13 +6,24 @@
 //
 
 import UIKit
+import iOSDropDown
 
 class AddVehicleController: UIViewController {
 
+    @IBOutlet weak var selectVehicleDropDown: DropDown!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        manageDropDown()
+    }
+    
+    func manageDropDown() {
+        selectVehicleDropDown.optionArray = ["SUV","Hatchback","Crossover","Convertible","Sedan","Sports Car","Coupe","Minivan","Station Wagon","Pickup Truck"]
+        selectVehicleDropDown.optionIds = [1,2,3,4,5,6,7,8,9,10]
+        selectVehicleDropDown.selectedRowColor = .gray
+        selectVehicleDropDown.didSelect{(selectedText , index ,id) in
+            print("Selected String: \(selectedText) \n index: \(index)")
+        }
     }
     
 
