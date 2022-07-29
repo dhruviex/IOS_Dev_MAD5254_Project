f9c0bbb1dd4a1f233e7be2613ac10ade52cd6a74
deekshith024 <deekshithreddy0001@gmail.com>
2022-07-29 11:32:55 -0400

    booking_collection_view_controller

---
+++ Parking Seeker/Parking Seeker/BookingCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BookingCollectionViewCell.swift
+//  Parking Seeker
+//
+//  Created by Karthik Reddy Rondla on 2022-07-29.
+//
+
+import UIKit
+
+class BookingCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    
+}
