f9bca63a5e281953775cbddd84b5acb49ffdd639
parthvekariya98 <parthvekariya260898@gmail.com>
2022-08-02 00:24:42 -0400

    - booking details screen ui design and managed booking selection navigation

--- Parking Seeker/Parking Seeker/BookingsViewController.swift
+++ Parking Seeker/Parking Seeker/BookingsViewController.swift
@@ -20,7 +20,10 @@ class BookingsViewController: UIViewController, UICollectionViewDelegate, UIColl
         // Do any additional setup after loading the view.
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
     //collection view datasource methods
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         test_booking_titles.count
@@ -29,8 +32,13 @@ class BookingsViewController: UIViewController, UICollectionViewDelegate, UIColl
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookingViewCell", for: indexPath) as! BookingViewCell
         cell.booking_title.text = test_booking_titles[indexPath.row]
-//        cell.booking_cell_view.cornerRadius = 20
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let BookingDetailController = UIStoryboard(name: "Main", bundle:nil).instantiateViewController(withIdentifier: "BookingDetailController") as! BookingDetailController
+        BookingDetailController.booking_title = test_booking_titles[indexPath.row]
+        self.navigationController?.pushViewController(BookingDetailController, animated: true)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
