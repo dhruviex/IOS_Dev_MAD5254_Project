1f2f5c3d933a704cbe2e70b101850ea09bea0177
Das101 <alishadas664@gmail.com>
2022-08-02 01:49:21 -0400

    - navigation action for my account screen

--- Parking Seeker/Parking Seeker/MyAccountController.swift
+++ Parking Seeker/Parking Seeker/MyAccountController.swift
@@ -15,15 +15,29 @@ class MyAccountController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //button action for navigation
+    @IBAction func myProfileBtnTapped(_ sender: Any) {
+        
     }
-    */
+    
+    @IBAction func paymentDetailsBtnTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func settingsBtnTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func helpBtnTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func contactUsBtnTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func aboutBtnTapped(_ sender: Any) {
+        
+    }
 
 }
