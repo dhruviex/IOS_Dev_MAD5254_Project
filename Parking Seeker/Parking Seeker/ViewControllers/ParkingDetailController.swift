ee7b8c0017c655d7aca5b80cc0ac74f2d9aac6c2
dhruviex <dhruviex7@gmail.com>
2022-08-08 00:59:14 -0400

    - parking details hours counter managed

--- Parking Seeker/Parking Seeker/ViewControllers/ParkingDetailController.swift
+++ Parking Seeker/Parking Seeker/ViewControllers/ParkingDetailController.swift
@@ -9,10 +9,35 @@ import UIKit
 
 class ParkingDetailController: UIViewController {
 
+    @IBOutlet weak var plusBtn: UIButton!
+    @IBOutlet weak var minusBtn: UIButton!
+    @IBOutlet weak var hoursLabel: UILabel!
+    
+    var currentHours = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.hoursLabel.text = "\(currentHours) Hour"
+    }
+    
+    @IBAction func plusBtnTap(_ sender: Any) {
+        currentHours = currentHours + 1
+        let hoursText = "\(currentHours) Hours"
+        self.hoursLabel.text = hoursText
+    }
+    
+    @IBAction func minusBtnTap(_ sender: Any) {
+        if (currentHours > 1) {
+            currentHours = currentHours - 1
+            var hoursText = ""
+            if (currentHours == 1) {
+                hoursText = "\(currentHours) Hour"
+            } else {
+                hoursText = "\(currentHours) Hours"
+            }
+            self.hoursLabel.text = hoursText
+        }
     }
     
     @IBAction func confirmBookingTap(_ sender: Any) {
